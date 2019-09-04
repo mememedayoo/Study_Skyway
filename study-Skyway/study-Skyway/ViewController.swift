323a595acb9229ea2105b02ea7e36af83696dcb1
mememedayoo <mememedayoo@gmail.com>
2019-09-04 18:54:02 +0900

    create project

---
+++ study-Skyway/study-Skyway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  study-Skyway
+//
+//  Created by 葛上海翔 on 2019/09/04.
+//  Copyright © 2019 kaito kuzukami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
